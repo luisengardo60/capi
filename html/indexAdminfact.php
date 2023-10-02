c3ab756c0299446bf7f1701609d9f835026cbb85
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2023-10-02 15:58:09 -0400

    subir_lunes02102023

--- html/indexAdminfact.php
+++ html/indexAdminfact.php
@@ -21,7 +21,7 @@
         <!--<div class="text"><a href="../html/index.html">C.A.P.I.</a></div>-->
         <ul>
             <li><a href="../inicio.php"><img src="../img/menu_inicio6.jpg" alt="C.A.P.I" style="padding-left:5x;padding-right:1px;padding-top: 1px; border-radius: 50px; float:center; width: 80px; height: 80px; background-color:green"></a></li>
-            <li><a href="#" class="feat-btn">Adminfact<span class="first">&#x021D3; </span></a>
+            <li><a href="#" class="feat-btn">Adminfact<br>Fritne Blanco<span class="first">&#x021D3; </span></a>
                 <ul class="feat-show">
                     <li><a href="../dataTables/organigrama.php">Organigrama</a></li>
                     <li><a href="../html/adminFactMenuServidores.html">Servidores</a></li>
@@ -40,13 +40,16 @@
                     <li><a href="../html/adminFactIncidencias.html">Incidencias</a></li>
                     <li><a href="../html/adminFactStatus.html">Status</a></li>
                     <li><a href="../html/adminFactDocMenu.html">Control-M Job's</a></li>
+                    <li><a href="../html/adminFactReplicaDaP.html">Replicate Tablas</a></li>
                 </ul>
             </li>
-            <!--<li><a href="../html/adminFactReplicateDaP.html">Proc. Replicate Tablas Des. a Prod. I</a></li>-->
-            <li><a href="../html/adminFactReplicaDaP.html">Replicate Tablas</a></li>
-            <li><a href="../html/adminfactChecklistCAPI.html">CheckList para recibir aplicativos en CAPI</a></li>
-            <li><a href="../html/Menu_Main1.html">Mainframe</a></li>
-            <!--<li><a href="https://ss64.com/" target="_blank" class="bg-linux">
+            <li><a href="#" class="serv-btn">Varios<span class="second">&#x021D3;</span></a>
+                <ul class="serv-show">
+                    <li><a href="../html/adminfactChecklistCAPI.html">CheckList para recibir aplicativos en CAPI</a></li>
+                    <li><a href="../html/Menu_Main1.html">Mainframe</a></li>
+                </ul>
+            </li>        
+                    <!--<li><a href="https://ss64.com/" target="_blank" class="bg-linux">
                     <img style="width: 50px; border-radius: 50%;" src="../img/linux.png" alt="Linux" /><span
                         class="segundo">Manual Online</span></a></li>
             <li><a href="https://www.bancodevenezuela.com/" target="_blank">
@@ -55,7 +58,7 @@
             <li><a href="https://www.mercantilbanco.com/" target="_blank">
                     <img class="mercantil" src="../img/mercantl.jpg" alt="Mercantil Banco" /><span class="segundo">Banco
                         Mercantil</span></a></li>-->
-        </ul>
+                </ul>
     </nav>
     <?php include '../titulo/titulo1.html' ?><br><br><br><br>
 
--- inicio.php
+++ inicio.php
@@ -17,31 +17,28 @@
     <nav>
         <?php include '../capi/titulo/titulo.html' ?>
 
-    </nav><br><br><br><br>
-    <div class="ini">
-        <a href="html/indexAdminfact.php"><button class="boton"><b>ADMINFACT</b></button></a>
-    </div>
+    </nav><br>
+    <div class="ini"></div>
+    <a href="html/indexAdminfact.php"><button class="boton"><b>ADMINFACT</b></button></a>
     <div class="ini1"></div>
     <a href="html/indexAdminBasis.php"><button class="boton"><b>ADMINBASIS</b></button></a>
     <div class="ini2"></div>
     <a href="html/indexAdminCrm.php"><button class="boton"><b>ADMINCRM</b></button></a>
     <div class="ini3"></div>
     <a href="html/indexAdminEdi.php"><button class="boton"><b>ADMINEDI</b></button></a>
-    <div class="ini4">
-        <a href="html/indexProyectos.php"><button class="boton"><b>PROYECTOS</b></button></a>
-    </div>
-    <div class="ini"></div>
+    <div class="ini4"></div>
+    <a href="html/indexProyectos.php"><button class="boton"><b>PROYECTOS</b></button></a>
+    <div class="ini5"></div>
     <a href="html/indexRecaudacion.php"><button class="boton"><b>FACTURACIÓN</b></button></a>
-    <div class="ini1"></div>
+    <div class="ini6"></div>
     <a href="html/indexEmpresarial.php"><button class="boton"><b>EMPRESARIAL</b></button></a>
-    <div class="ini2"></div>
+    <div class="ini7"></div>
     <a href="html/indexAprovision.php"><button class="boton"><b>APROVISIONAMIENTO</b></button></a>
+    <div class="ini8"></div>
     <a href="html/indexTecnologia.php"><button class="boton"><b>TECNOLOGÌA</b></button></a>
-    <div class="ini3"></div>
+    <div class="ini9"></div>
     <a href="login.html"><button class="boton"><b>SALIR</b></button></a>
-    </div>
-
-
+</nav>
 
     <article>
         <?php include 'reloj/reloj.html' ?>
